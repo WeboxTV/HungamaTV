daf6cccb9b61e68f22fcf3c41bc10dd6ebd1931b
AS <59010081+iArchit@users.noreply.github.com>
2020-07-14 14:03:05 +0700

    Create index.js

---
+++ Live/index.js
@@ -0,0 +1,80 @@
+var d = new Date(); // get date 
+var h = d.getHours(); // get hour in 24
+
+var player = document.getElementById('player');
+
+switch (h) {
+
+  case 1:
+    player.src = 'https://download.blender.org/durian/trailer/sintel_trailer-720p.mp4';
+    break;
+  case 2:
+    player.src = 'https://download.blender.org/durian/trailer/sintel_trailer-720p.mp4';
+  case 3:
+    player.src = 'https://download.blender.org/durian/trailer/sintel_trailer-720p.mp4';
+    break;
+  case 4:
+    player.src = 'https://download.blender.org/durian/trailer/sintel_trailer-720p.mp4';
+    break;
+  case 5:
+    player.src = 'https://download.blender.org/durian/trailer/sintel_trailer-720p.mp4';
+    break;
+  case 6:
+    player.src = 'https://download.blender.org/durian/trailer/sintel_trailer-720p.mp4';
+    break;
+  case 7:
+    player.src = 'https://download.blender.org/durian/trailer/sintel_trailer-720p.mp4';
+    break;
+  case 8:
+    player.src = 'https://download.blender.org/durian/trailer/sintel_trailer-720p.mp4';
+    break;
+  case 9:
+    player.src = 'https://download.blender.org/durian/trailer/sintel_trailer-720p.mp4';
+    break;
+  case 10:
+    player.src = 'https://download.blender.org/durian/trailer/sintel_trailer-720p.mp4';
+    break;
+  case 11:
+    player.src = 'https://download.blender.org/durian/trailer/sintel_trailer-720p.mp4';
+
+    break;
+  case 12:
+    player.src = 'https://download.blender.org/durian/trailer/sintel_trailer-720p.mp4';
+    break;
+  case 13:
+    player.src = 'https://download.blender.org/durian/trailer/sintel_trailer-720p.mp4';
+    break;
+  case 14:
+    player.src = 'https://download.blender.org/durian/trailer/sintel_trailer-720p.mp4';
+    break;
+  case 15:
+    player.src = 'https://download.blender.org/durian/trailer/sintel_trailer-720p.mp4';
+    break;
+  case 16:
+    player.src = 'https://download.blender.org/durian/trailer/sintel_trailer-720p.mp4';
+    break;
+  case 17:
+    player.src = 'https://download.blender.org/durian/trailer/sintel_trailer-720p.mp4';
+    break;
+  case 18:
+    player.src = 'https://download.blender.org/durian/trailer/sintel_trailer-720p.mp4';
+    break
+  case 19:
+    player.src = 'https://download.blender.org/durian/trailer/sintel_trailer-720p.mp4';
+    break;
+  case 20:
+    player.src = 'https://download.blender.org/durian/trailer/sintel_trailer-720p.mp4';
+    break;
+  case 21:
+    player.src = 'https://download.blender.org/durian/trailer/sintel_trailer-720p.mp4';
+    break;
+  case 22:
+    player.src = 'https://download.blender.org/durian/trailer/sintel_trailer-720p.mp4';
+    break;
+  case 23:
+    player.src = 'https://download.blender.org/durian/trailer/sintel_trailer-720p.mp4';
+    break;
+  case 0:
+    player.src = 'https://download.blender.org/durian/trailer/sintel_trailer-720p.mp4';
+    break;
+}
